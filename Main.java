227ddd4c9d780c4d4583e3dbf6e2244e9188a029
Jordan Jalles <jjalles@indiana.edu>
2012-09-26 20:56:24 -0400

    takes a while to run...but the results ='D

--- Main.java
+++ Main.java
@@ -9,6 +9,9 @@ public class Main{
     public static void main(String[] args) throws Exception{
 	String instructions = dragon();
 	Pen pen = new Pen(500, 500, instructions);
+	pen.blueShift = (float)0.01;
+	pen.redShift = (float)0.02;
+	//pen.greenShift = 3;
 
 	pen.draw();
 
@@ -19,7 +22,7 @@ public class Main{
 	Dictionary dict = new Dictionary();
 	dict.takeRule("X", "X+YF");
 	dict.takeRule("Y", "FX-Y");
-	return instructions(dict, axiom, 10);
+	return instructions(dict, axiom, 17);
     }
 
     public static String instructions(Dictionary dict, String axiom, int iterations){
@@ -28,9 +31,11 @@ public class Main{
 	for (int i = 0; i<iterations; i++){
 	    for (int j = 0; j<instructions.length(); j++){
 		nextResult += (dict.lookUp(Character.toString(instructions.charAt(j))));
+		System.out.println(((float)j)/instructions.length());
 	    }
 	    instructions = nextResult;
 	    nextResult = "";
+
 	}
 	return instructions;
     }
--- Pen.java
+++ Pen.java
@@ -7,81 +7,85 @@ import javax.swing.*;
 
 public class Pen{
 
-    private int x,y;
-    private String instructions;
-    private Cardinal direction = Cardinal.NORTH;
-    private Graphics g;
+  private int x,y;
+  private String instructions;
+  private Cardinal direction = Cardinal.NORTH;
+  private Graphics g;
 
-    public int stepSize = 1;
-    public int red = 255;
-    public int green = 255;
-    public int blue = 255;
-    public int redShift = 0;
-    public int greenShift = 0;
-    public int blueShift = 0;
-    public Color color = new Color(red, green, blue);
-    
+  public int stepSize = 1;
+  public float red = 255;
+  public float green = 255;
+  public float blue = 255;
+  private float redBuff = 0;
+  private float greenBuff = 0;
+  private float blueBuff = 0;
+  public float redShift = 0;
+  public float greenShift = 0;
+  public float blueShift = 0;
+  public Color color = new Color((int)red, (int)green, (int)blue);
 
-    public Pen(int x, int y, String instructions){
-	this.x = x;
-	this.y = y;
-	this.instructions = instructions;	
+
+  public Pen(int x, int y, String instructions){
+    this.x = x;
+    this.y = y;
+    this.instructions = instructions;
+  }
+
+  public void draw() throws Exception{
+    createImage("dragon");
+  }
+
+
+  private BufferedImage createImage(String fileName) throws IOException{
+
+    BufferedImage img = new BufferedImage(1000, 1000, BufferedImage.TYPE_INT_RGB);
+    img.createGraphics();
+    this.g = img.createGraphics();
+    g.setColor(color);
+
+    doInstructions();
+    Pen.writeImage(img, fileName);
+
+    return img;
+  }
+
+  private static void writeImage(BufferedImage image, String fileName) throws IOException{
+    if (fileName == null) return;
+
+    int offset = fileName.lastIndexOf(".");
+    String type = offset == -1? "bmp" : fileName.substring(offset + 1);
+
+    ImageIO.write(image, type, new File(fileName));
+  }
+  private void forward(){
+    int[] dxy = direction.getValue();
+    redBuff+=redShift;
+    greenBuff += greenShift;
+    blueBuff += blueShift;
+    color = new Color((int)(red+redBuff)%255, (int)(green+greenBuff)%255, (int)(blue+blueBuff)%255);
+    g.setColor(color);
+
+    for (int i = 0; i < stepSize; i++){
+      g.drawLine(x,y,x+1,y+1);
+      x += dxy[0];
+      y += dxy[1];
     }
+  }
+  private void doInstructions(){
+    for (int i = 0; i < instructions.length(); i++){
+      System.out.println(i/instructions.length());
+      char c = instructions.charAt(i);
+      switch(c){
 
-    public void draw() throws Exception{
-	createImage("dragon");
+      case 'F':  forward(); break;
+      case '-': direction = direction.left(); break;
+      case '+': direction = direction.right(); break;
+      default: break;
+
+      }
     }
+  }
 
 
-    private BufferedImage createImage(String fileName) throws IOException{
 
-	BufferedImage img = new BufferedImage(1000, 1000, BufferedImage.TYPE_INT_RGB);
-	img.createGraphics();
-	this.g = img.createGraphics();
-	g.setColor(color);
-
-	doInstructions();
-	Pen.writeImage(img, fileName);
-
-	return img;
-    }
-
-    private static void writeImage(BufferedImage image, String fileName) throws IOException{
-	if (fileName == null) return;
-
-	int offset = fileName.lastIndexOf(".");
-	String type = offset == -1? "bmp" : fileName.substring(offset + 1);
-
-	ImageIO.write(image, type, new File(fileName));
-    }
-    private void forward(){
-	int[] dxy = Cardinal.NORTH.getValue();
-
-	red = (red + redShift)%255;
-	green = (green + greenShift)%255;
-	blue = (blue + blueShift)%255;
-	color = new Color(red, green, blue);
-
-	for (int i = 0; i < stepSize; i++){
-	    g.drawLine(x,y,x+1,y+1);
-	    x += dxy[0];
-	    y += dxy[1];
-	}
-    }
-    private void doInstructions(){
-	for (int i = 0; i < instructions.length(); i++){
-	    char c = instructions.charAt(i);
-	    switch(c){
-
-	    case 'F':  forward(); break;
-	    case '-': direction = direction.left(); break;
-	    case '+': direction = direction.right(); break;
-	    default: break;
-
-	    }
-	}
-    }
-    
-    
-    
 }
